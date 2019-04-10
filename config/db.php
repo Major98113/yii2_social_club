ede0a5bdf98d25a1ed3d8d86ca02f0d8128a2f73
max <meison98@bk.ru>
2019-04-10 18:11:34 +0300

    сделал регистрацию, авторизацию

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii_social_club',
     'username' => 'root',
     'password' => '1234',
     'charset' => 'utf8',
